05f6b701afe69682442a8d35ab55f9837b81f248
GAUTHIER <gauthierdewit@gmail.com>
2021-02-26 10:46:07 +0100

    j.s.

---
+++ public/javascript/main.js
@@ -0,0 +1,34 @@
+// Variables
+//button Color
+let btnGrey =document.querySelector(".btn-secondary")
+let btnDanger =document.querySelector(".btn-danger")
+let btnSuccess =document.querySelector(".btn-success")
+let btnPurple =document.querySelector(".btnPuple")
+
+// Button Border
+let brdGros =document.querySelector(".brdGros")
+let brdDash =document.querySelector(".brdDash")
+let brdPetit =document.querySelector(".brdPetit")
+let brdVide =document.querySelector(".brdVide")
+let brdTop2 =document.querySelector(".brdTop2")
+let brdTop6 =document.querySelector(".brdTop6")
+
+// Input Color & Border
+let inpColor =document.querySelector(".inpColor")
+let inp =document.querySelector(".inp")
+let inpTop =document.querySelector(".inpTop")
+let inpRight =document.querySelector(".inpRight")
+let inpBottom =document.querySelector(".inpBottom")
+let inpLeft =document.querySelector(".inpLeft")
+
+//input Radius
+let radiusFull =document.querySelector(".radiusFull")
+let leftTop =document.querySelector(".leftTop")
+let rightTop =document.querySelector(".rightTop")
+let rightBottom =document.querySelector(".rightBottom")
+let leftBottom =document.querySelector(".leftBottom")
+
+// PLan
+let plan = document.querySelector(".plan")
+
+//addEventList
